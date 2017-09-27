d4bd01cc1d76a30156aa24b5203f8da492d567f0
Abby Christens <achristens@gmail.com>
2017-09-27 16:14:38 -0400

    Adding delete methods for class and instance levels

--- contact.rb
+++ contact.rb
@@ -2,31 +2,39 @@ class Contact
 
   @@contacts = []
   @@id       = 1
-  
+
   # This method should initialize the contact's attributes
   def initialize(first_name, last_name, email, notes = 'N/A')
     @first_name = first_name
     @last_name  = last_name
     @email      = email
     @notes      = notes
+
     @id         = @@id
     @@id       += 1
   end
 
   # This method should call the initializer,
   # store the newly created contact, and then return it
-  def self.create
-
+  def self.create(first_name, last_name, email, notes = 'N/A')
+    new_contact = Contact.new(first_name, last_name, email, notes)
+    @@contacts << new_contact
+    return new_contact
   end
 
   # This method should return all of the existing contacts
   def self.all
-
+    return @@contacts
   end
 
   # This method should accept an id as an argument
   # and return the contact who has that id
-  def self.find
+  def self.find(id)
+    # if @@contacts.include?(id)
+    #   puts "yes they're here"
+    # else
+    #   puts "Not on the list"
+    # end
 
   end
 
@@ -48,7 +56,14 @@ class Contact
 
   # This method should delete all of the contacts
   def self.delete_all
-
+    puts "Are you sure you want to delete all contacts?"
+    answer = gets.chomp
+    if answer == "yes" || answer == "true"
+       @@contacts.clear
+       puts "Your contact list is now empty"
+    else
+      puts "No contacts have been deleted"
+    end
   end
 
   def full_name
@@ -58,7 +73,8 @@ class Contact
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
-
+    @@contacts.delete(self)
+    return @@contacts
   end
 
   # Feel free to add other methods here, if you need them.
@@ -95,8 +111,12 @@ class Contact
   def notes
     @notes
   end
+
+  def id
+    @id
+  end
 end
 
-# abby = Contact.new("abby", "christens", 'abby@gmail.com')
+# abby = Contact.create("abby", "christens", 'abby@gmail.com')
 # abby.first_name = "Abigail"
 # puts abby.first_name
